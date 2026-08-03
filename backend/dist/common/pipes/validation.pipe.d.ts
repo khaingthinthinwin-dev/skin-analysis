0b7838ed4afa511391ffa8fa6468599be7c47b9b
mdcrshinminthant <shinminthant@myanmardcr.com>
2026-08-03 14:31:44 +0630

    chore: remove tracked dist/ files from git

--- backend/dist/common/pipes/validation.pipe.d.ts
+++
@@ -1,5 +0,0 @@
-import { PipeTransform } from '@nestjs/common';
-export declare class ValidationPipe implements PipeTransform<any> {
-    transform(value: any, { metatype }: any): Promise<any>;
-    private toValidate;
-}
